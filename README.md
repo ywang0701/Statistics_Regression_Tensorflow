88ef7b509b7df4cccd696ffc93e4964ed4c47491
ywang0701 <ywangperl@gmail.com>
2017-09-13 20:48:37 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,18 @@
+"# Statistics_Regression_Tensorflow" 
+
+# What is this?
+    The main.py is the python script that use tensorflow and do multiple regression.
+
+## Dependencies
+- Install python3.5
+- Install tensorflow and pandas: pip install tensorflow pandas
+
+## How to run the script?
+- python main.py [data csv file]
+
+  where data_csv_file must have values of dependent variables in formerly columns and values of independent variable in last column.
+
+
+## Graph and Diagram.
+   Go to project folder and run this command: tensorboard --logdir ./logs --port 6006
+   And then web borwer, go to localhost:6006
---
+++ main.py
@@ -0,0 +1,72 @@
+# /usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+def main():
+    # placeholders for a tensor that will be always fed.
+    X = tf.placeholder(tf.float32, shape=[None, 7])
+    Y = tf.placeholder(tf.float32, shape=[None, 1])
+    # Y_hist = tf.summary.histogram('Y', Y)
+
+    W = tf.Variable(tf.random_normal([7, 1]), name='weight')
+    b = tf.Variable(tf.random_normal([1]), name='bias')
+    W_hist  = tf.summary.histogram('W', W)
+    b_hist = tf.summary.histogram('b', b)
+
+    # Hypothesis
+    hypothesis = tf.matmul(X, W) + b
+    # hypothesis_hist = tf.summary.histogram("hypothesis", hypothesis)
+
+    # Simplified cost/loss function
+    cost = tf.reduce_mean(tf.square(hypothesis - Y))
+    cost_scalr = tf.summary.scalar('cost', cost)
+
+    # Minimize
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
+    train = optimizer.minimize(cost)
+
+    # Launch the graph in a session.
+    sess = tf.Session()
+    merged_summary = tf.summary.merge_all()
+    writer = tf.summary.FileWriter("./logs")
+    writer.add_graph(sess.graph)  # Show the graph
+
+    # Initializes global variables in the graph.
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(100000):
+        summary, cost_val, hy_val, W_val, b_val, _ = sess.run(
+            [merged_summary, cost, hypothesis, W, b, train], feed_dict={X: x_data, Y: y_data})
+        writer.add_summary(summary, global_step=step)
+
+        if step % 10 == 0:
+            # print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
+            print(step, "Cost: ", cost_val)
+            print("b: ", b_val)
+            print ("W\n", W_val)
+
+    # Ask my score
+    # print("Your score will be ", sess.run(
+    #     hypothesis, feed_dict={X: [[100, 70, 101]]}))
+    #
+    # print("Other scores will be ", sess.run(hypothesis,
+    #                                         feed_dict={X: [[60, 70, 110], [90, 100, 80]]}))
+
+if __name__ == "__main__":
+    tf.set_random_seed(777)  # for reproducibility
+
+    try:
+        csv_file = sys.argv[2]
+    except:
+        # sys.exit("Not specified data file")
+        csv_file = '00_nom_135_records.csv'
+
+    xy = pd.read_csv(csv_file, delimiter=',', dtype=np.float32).as_matrix(columns=None)
+
+    x_data = xy[:, 0:-1] # values of dependent variables
+    y_data = xy[:, [-1]] # values of independent variable
+
+    main()
